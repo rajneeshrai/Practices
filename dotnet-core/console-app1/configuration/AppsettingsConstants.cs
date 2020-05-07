f1807104aba286e85d44723432e8277a565b55ff
rajneeshrai <rajneeshrai83@yahoo.in>
2020-05-07 18:54:12 +0530

    utility for update folders/files to lower case

---
+++ dotnet-core/console-app1/configuration/AppsettingsConstants.cs
@@ -0,0 +1,8 @@
+namespace console_app1
+{
+    public static class Appsettings_Constants
+    {
+        public const string SourceFolderPath = nameof(SourceFolderPath);
+        public const string IgnoreFolders = nameof(IgnoreFolders);
+    }
+}
